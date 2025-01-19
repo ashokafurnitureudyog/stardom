9ce3c6e034a3123cfebe6467a93efb8110413f8c
Abhishek Sharma <abhitiku2003@gmail.com>
2025-01-19 18:34:48 +0530

    Update package dependencies and add BasicCompanyInfo constant

---
+++ src/lib/constants/CompanyInfo.ts
@@ -0,0 +1,14 @@
+export const BasicCompanyInfo = {
+  name: "Stardom",
+  parentCompany: "Ashoka Furniture Udyog",
+  established: "1996",
+  address: {
+    street: "Plot No. 304, Industrial Area Phase 2",
+    city: "Chandigarh",
+    Country: "India",
+    zip: "160002",
+  },
+  phone: "+91 62846 73783",
+  email: "hello@stardom.co.in",
+  website: "https://stardom.co.in/",
+};
